6b88b3c19603a52071a6e79a80caf6d57ec7bc14
serhiiur <serhiiur@duck.com>
2026-05-23 18:38:10 +0300

    Update assertions

--- fastapi-boilerplate/test_main.py
+++ fastapi-boilerplate/test_main.py
@@ -8,8 +8,6 @@ from httpx import ASGITransport, AsyncClient
 
 from main import ApiHealth, ApiVersion, app  # isort: skip
 
-pytestmark = pytest.mark.anyio
-
 
 # Override application state objects
 app.state.logger = logging.getLogger(__name__)
@@ -17,27 +15,31 @@ app.state.logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope="session")
 def anyio_backend() -> str:
-  """Backend (asyncio) for pytest to run async tests."""
-  return "asyncio"
+    """Backend (asyncio) for pytest to run async tests."""
+    return "asyncio"
 
 
 @pytest.fixture(scope="session")
 async def client() -> AsyncIterator[AsyncClient]:
-  """Fixture to provide async HTTP client for API endpoints."""
-  async with LifespanManager(app) as manager:
-    transport = ASGITransport(manager.app)
-    base_url = "http://test"
-    async with AsyncClient(base_url=base_url, transport=transport) as client:
-      yield client
+    """Fixture to provide async HTTP client for API endpoints."""
+    async with LifespanManager(app) as manager:
+        transport = ASGITransport(manager.app)
+        base_url = "http://test"
+        async with AsyncClient(base_url=base_url, transport=transport) as client:
+            yield client
 
 
 async def test_health(client: AsyncClient) -> None:
-  resp = await client.get("/api/health")
-  assert resp.status_code == status.HTTP_200_OK
-  assert resp.json() == ApiHealth.model_construct().model_dump()
+    resp = await client.get("/api/health")
+    assert resp.status_code == status.HTTP_200_OK
+    resp_json = resp.json()
+    assert "timestamp" in resp_json
+    assert resp_json["status"] == ApiHealth().status
 
 
 async def test_version(client: AsyncClient) -> None:
-  resp = await client.get("/api/version")
-  assert resp.status_code == status.HTTP_200_OK
-  assert resp.json() == ApiVersion.model_construct().model_dump()
+    resp = await client.get("/api/version")
+    assert resp.status_code == status.HTTP_200_OK
+    resp_json = resp.json()
+    assert "timestamp" in resp_json
+    assert resp_json["version"] == ApiVersion().version
